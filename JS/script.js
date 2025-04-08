d4cb32dc1c40c91063de924189dd5714a039fa25
Kaushik105 <storakaushik001@gmail.com>
2025-04-08 19:56:53 +0530

    the click feature

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,14 @@ let svgs = Array.from(document.querySelectorAll(".faqbox svg"));
 svgs.forEach((e) => {
   e.addEventListener("click", (e) => {
     const siblingDiv = e.target.parentNode.parentNode.children[1]; // Gets the second child of grandparent
-    if (siblingDiv.style.display == "block") {
-      siblingDiv.style.display = "none";
-      siblingDiv.style.opacity = 0;
-    } else {
-      siblingDiv.style.opacity = 1;
-      siblingDiv.style.display = "block";
+    if (Array.from(siblingDiv.classList)[0] === "infoTile") {
+      if (siblingDiv.style.display === "block") {
+        siblingDiv.style.display = "none";
+        siblingDiv.style.opacity = 0;
+      } else {
+        siblingDiv.style.opacity = 1;
+        siblingDiv.style.display = "block";
+      }
     }
-    console.log(siblingDiv.ClassList)
   });
 });
