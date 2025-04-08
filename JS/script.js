8b4a63a0ef0036f4cb81996a59c936a8218d6485
Kaushik105 <storakaushik001@gmail.com>
2025-04-08 21:24:35 +0530

    Enhance FAQ interaction: update SVG click behavior and improve visibility transitions

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,29 @@
 let svgs = Array.from(document.querySelectorAll(".faqbox svg"));
+const otherTiles = Array.from(document.querySelectorAll(".infoTile"));
 
-svgs.forEach((e) => {
-  e.addEventListener("click", (e) => {
+svgs.forEach((svg) => {
+  svg.style.transform = "rotateZ(0deg)";
+  svg.addEventListener("click", (e) => {
     const siblingDiv = e.target.parentNode.parentNode.children[1]; // Gets the second child of grandparent
-    if (Array.from(siblingDiv.classList)[0] === "infoTile") {
-      if (siblingDiv.style.display === "block") {
-        siblingDiv.style.display = "none";
-        siblingDiv.style.opacity = 0;
-      } else {
+    otherTiles.forEach((item) => {
+      if (siblingDiv != item) {
+        item.style.visibility = "hidden";
+        item.style.position = "absolute";
+        item.style.opacity = 0;
+      }
+    });
+
+    if (siblingDiv) {
+      if (siblingDiv.style.visibility !== "visible") {
         siblingDiv.style.opacity = 1;
-        siblingDiv.style.display = "block";
+        siblingDiv.style.position = "static";
+        siblingDiv.style.visibility = "visible";
+        e.target.style.transform = "rotate(45deg)";
+      } else {
+        siblingDiv.style.visibility = "hidden";
+        siblingDiv.style.position = "absolute";
+        siblingDiv.style.opacity = 0;
+        e.target.style.transform = "rotate(0deg)";
       }
     }
   });
